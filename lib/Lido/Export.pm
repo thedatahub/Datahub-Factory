1f5249e7d0013c6649e700cc50c8592664d3b4f3
Pieter De Praetere <pieter@packed.be>
2016-12-12 10:27:10 +0100

    Added optional file_name

--- lib/Lido/Export.pm
+++ lib/Lido/Export.pm
@@ -4,13 +4,18 @@ use Moo;
 use Catmandu;
 use strict;
 
-has file_name => (is => 'ro', required => 1);
+has file_name => (is => 'ro');
 
 has out  => (is => 'lazy');
 
 sub _build_out {
     my $self = shift;
-    my $exporter = Catmandu->exporter('LIDO');
+    my $exporter;
+    if (defined($self->file_name)) {
+        $exporter = Catmandu->exporter('LIDO', file => $self->file_name);
+    } else {
+        $exporter = Catmandu->exporter('LIDO');
+    }
     return $exporter;
 }
 
