38d28f62ad5b6a6701a678d6e09f7a5258c4dea3
Pieter De Praetere <pieter@packed.be>
2016-12-12 13:39:00 +0100

    Added temporary tables for lookups

--- lib/TMS/Import.pm
+++ lib/TMS/Import.pm
@@ -15,7 +15,7 @@ has importer  => (is => 'lazy');
 sub _build_importer {
     my $self = shift;
     my $dsn = sprintf('dbi:mysql:%s', $self->db_name);
-    my $query = 'select * from objects where ClassificationID <> 0 limit 10';
+    my $query = 'select * from objects';
     my $importer = Catmandu->importer('DBI', dsn => $dsn, host => $self->db_host, user => $self->db_user, password => $self->db_password, query => $query);
     $self->prepare();
     return $importer;
@@ -24,31 +24,41 @@ sub _build_importer {
 sub prepare {
     my $self = shift;
     $self->__classifications();
+    $self->__period();
 }
 
-sub __classifications {
-    my $self = shift;
-    #catmandu import DBI --dsn "dbi:mysql:tms_kmska" --user datahub --password datahub --query "select ClassificationID as _id, Classification as term from Classifications" to DBI --data_source "dbi:SQLite:/vagrant/Datahub-Fixes/kmska_classifications.sqlite"
+sub prepare_call {
+    my ($self, $import_query, $store_table) = @_;
     my $importer = Catmandu->importer(
         'DBI',
         dsn      => sprintf('dbi:mysql:%s', $self->db_name),
         host     => $self->db_host,
         user     => $self->db_user,
         password => $self->db_password,
-        query    => 'select ClassificationID as _id, Classification as term from Classifications'
+        query    => $import_query
     );
     my $store = Catmandu->store(
         'DBI',
-        data_source => 'dbi:mysql:datahub',
+        data_source => sprintf('dbi:SQLite:/tmp/tms_import.%s.sqlite', $store_table),
         host        => $self->db_host,
         username    => $self->db_user,
         password    => $self->db_password
     );
     $importer->each(sub {
         my $item = shift;
-        my $bag = $store->bag('classifications');
+        my $bag = $store->bag();
         $bag->add($item);
     });
+}
+
+sub __classifications {
+    my $self = shift;
+    $self->prepare_call('select ClassificationID as _id, Classification as term from Classifications', 'classifications');
+}
+
+sub __period {
+    my $self = shift;
+    $self->prepare_call('select ObjectID as _id, Period as term from ObjContext', 'periods')
 }
 
 1;
