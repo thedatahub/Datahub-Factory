e7ebe2e043bd83a5c4111234f3950d8730772dcb
Pieter De Praetere <pieter@packed.be>
2017-02-22 12:04:32 +0100

    Updated documentation

--- README.md
+++ README.md
@@ -72,147 +72,16 @@ options.
 ## transport \[OPTIONS\]
 
 Fetch data from a local or remote source, convert it to an exchange format and
-push the data to a Datahub instance.
+export the data.
 
-### Command line options
+[Datahub::Factory::Command::transport](https://metacpan.org/pod/Datahub::Factory::Command::transport)
 
-- `--importer NAME`
+## merge \[OPTIONS\]
 
-    The importer which fetches data from a Collection Registration system.
-    Currently only "Adlib" and "TMS" are supported options.
-    All `--oimport` arguments are tied to the specific importer used.
+Fetch data from two sources, convert it to an exchange format, merge the
+two records and export the data.
 
-- `--fixes PATH`
-
-    The path to the Catmandu Fix files to transform the data.
-
-- `--exporter NAME`
-
-    The exporter that will do something with your data. It is possible to
-    print to `STDOUT` in a specific format ("YAML" and "LIDO" are supported)
-    or to export to a Datahub instance.
-    All `--oexport` arguments are tied to the specific exporter used.
-
-- `--oimport file_name=PATH`
-
-    The path to a flat file containing data. This option is only relevant when
-    the input is an Adlib XML export file.
-
-- `--oimport db_user=VALUE`
-
-    The database user. This option is only relevant when
-    the input is an TMS database.
-
-- `--oimport db_passowrd=VALUE`
-
-    The database user password. This option is only relevant when
-    the input is an TMS database.
-
-- `--oimport db_name=VALUE`
-
-    The database name. This option is only relevant when
-    the input is an TMS database.
-
-- `--oimport db_host=VALUE`
-
-    The database host. This option is only relevant when
-    the input is an TMS database.
-
-- `--oexport datahub_url=VALUE`
-
-    The URL to the datahub instance. This should be a FQDN ie. http://datahub.lan/
-
-- `--oexport oauth_client_id=VALUE`
-
-    The client public ID. Used for OAuth authentication of the Datahub endpoint.
-
-- `--oexport oauth_client_secret=VALUE`
-
-    The client secret passphrase. Used for OAuth authentication of the Datahub
-    endpoint.
-
-- `--oexport oauth_username=VALUE`
-
-    The username of the Datahub user. Used for OAuth authentication of the Datahub
-    endpoint.
-
-- `--oexport oauth_password=VALUE`
-
-    The password of the Datahub user. Used for OAuth authentication of the Datahub
-    endpoint.
-
-### Pipeline configuration file
-
-The _pipeline configuration file_ is in the [INI format](http://search.cpan.org/~sherzodr/Config-Simple-4.59/Simple.pm#INI-FILE) and its location is
-provided to the application using the `--pipeline` switch.
-
-The file is broadly divided in two parts: the first (shortest) part configures
-the pipeline itself and sets the plugins to use for the _import_, _fix_ and
-_export_ actions. The second part sets options specific for the used plugins.
-
-#### Pipeline configuration
-
-This part has three sections: `[Importer]`, `[Fixer]` and `[Exporter]`.
-Every section has just one option: `plugin`. Set this to the plugin you
-want to use for every action.
-
-All current supported plugins are in the `Importer` and `Exporter` folders.
-For the `[Fixer]`, only the _Fix_ plugin is supported.
-
-The `[Fixer]` has an additional option, `id_path`. This options contains the
-path (in Fix syntax) of the identifier of each record in your data after the
-fix has been applied, but before it is submitted to the _Exporter_. It is used
-for reporting and logging.
-
-Supported _Importer_ plugins:
-
-- [TMS](https://metacpan.org/pod/Datahub::Factory::Importer::TMS)
-- [Adlib](https://metacpan.org/pod/Datahub::Factory::Importer::Adlib)
-- [KMSKA](https://metacpan.org/pod/Datahub::Factory::Importer::KMSKA)
-- [MSK](https://metacpan.org/pod/Datahub::Factory::Importer::MSK)
-- [VKC](https://metacpan.org/pod/Datahub::Factory::Importer::VKC)
-- [OAI](https://metacpan.org/pod/Datahub::Factory::Importer::OAI)
-
-Supported _Exporter_ plugins:
-
-- [Datahub](https://metacpan.org/pod/Datahub::Factory::Exporter::Datahub)
-- [LIDO](https://metacpan.org/pod/Datahub::Factory::Exporter::LIDO)
-- [YAML](https://metacpan.org/pod/Datahub::Factory::Exporter::YAML)
-
-#### Plugin configuration
-
-All plugins have their own configuration options in sections called
-`[plugin_type_name]` where `type` can be _importer_, _exporter_
-or _fixer_ and `name` is the name of the plugin (see above).
-
-For a list of supported and required options, see the plugin documentation.
-
-#### Example configuration file
-
-    [Importer]
-    plugin = Adlib
-
-    [Fixer]
-    plugin = Fix
-    id_path = 'administrativeMetadata.recordWrap.recordID.0._'
-
-    [Exporter]
-    plugin = Datahub
-
-    [plugin_importer_Adlib]
-    file_name = '/tmp/adlib.xml'
-    data_path = 'recordList.record.*'
-
-    [plugin_fixer_Fix]
-    fix_file = '/tmp/msk.fix'
-
-    [plugin_exporter_Datahub]
-    datahub_url = my.thedatahub.io
-    datahub_format = LIDO
-    oauth_client_id = datahub
-    oauth_client_secret = datahub
-    oauth_username = datahub
-    oauth_password = datahub
+[Datahub::Factory::Command::merge](https://metacpan.org/pod/Datahub::Factory::Command::merge)
 
 # AUTHORS
 
--- lib/Datahub/Factory/Command/transport.pm
+++ lib/Datahub/Factory/Command/transport.pm
@@ -115,28 +115,188 @@ This command allows datamanagers to (a) fetch data from a (local) source (b)
 transform the data to LIDO using a fix (c) upload the LIDO transformed data to
 a Datahub instance.
 
-=head1 METHODS
+=head1 COMMAND LINE INTERFACE
 
-=head2 abstract
+=head2 Options
 
- abstract();
+=over
 
-=head2 description
+=item C<--importer NAME>
 
- description();
+The importer which fetches data from a Collection Registration system.
+Currently only "Adlib" and "TMS" are supported options.
+All C<--oimport> arguments are tied to the specific importer used.
 
-=head2 execute
+=item C<--fixes PATH>
 
- execute();
+The path to the Catmandu Fix files to transform the data.
 
-=head2 opt_spec
+=item C<--exporter NAME>
 
- opt_spec();
+The exporter that will do something with your data. It is possible to
+print to C<STDOUT> in a specific format ("YAML" and "LIDO" are supported)
+or to export to a Datahub instance.
+All C<--oexport> arguments are tied to the specific exporter used.
 
-=head2 validate_args
+=item C<--oimport file_name=PATH>
 
- validate_args();
+The path to a flat file containing data. This option is only relevant when
+the input is an Adlib XML export file.
 
+=item C<--oimport db_user=VALUE>
+
+The database user. This option is only relevant when
+the input is an TMS database.
+
+=item C<--oimport db_passowrd=VALUE>
+
+The database user password. This option is only relevant when
+the input is an TMS database.
+
+=item C<--oimport db_name=VALUE>
+
+The database name. This option is only relevant when
+the input is an TMS database.
+
+=item C<--oimport db_host=VALUE>
+
+The database host. This option is only relevant when
+the input is an TMS database.
+
+=item C<--oexport datahub_url=VALUE>
+
+The URL to the datahub instance. This should be a FQDN ie. http://datahub.lan/
+
+=item C<--oexport oauth_client_id=VALUE>
+
+The client public ID. Used for OAuth authentication of the Datahub endpoint.
+
+=item C<--oexport oauth_client_secret=VALUE>
+
+The client secret passphrase. Used for OAuth authentication of the Datahub
+endpoint.
+
+=item C<--oexport oauth_username=VALUE>
+
+The username of the Datahub user. Used for OAuth authentication of the Datahub
+endpoint.
+
+=item C<--oexport oauth_password=VALUE>
+
+The password of the Datahub user. Used for OAuth authentication of the Datahub
+endpoint.
+
+=back
+
+=head2 Pipeline configuration file
+
+The I<pipeline configuration file> is in the L<INI format|http://search.cpan.org/~sherzodr/Config-Simple-4.59/Simple.pm#INI-FILE> and its location is
+provided to the application using the C<--pipeline> switch.
+
+The file is broadly divided in two parts: the first (shortest) part configures
+the pipeline itself and sets the plugins to use for the I<import>, I<fix> and
+I<export> actions. The second part sets options specific for the used plugins.
+
+=head4 Pipeline configuration
+
+This part has three sections: C<[Importer]>, C<[Fixer]> and C<[Exporter]>.
+Every section has just one option: C<plugin>. Set this to the plugin you
+want to use for every action.
+
+All current supported plugins are in the C<Importer> and C<Exporter> folders.
+For the C<[Fixer]>, only the I<Fix> plugin is supported.
+
+Supported I<Importer> plugins:
+
+=over
+
+=item L<TMS|Datahub::Factory::Importer::TMS>
+
+=item L<Adlib|Datahub::Factory::Importer::Adlib>
+
+=item L<KMSKA|Datahub::Factory::Importer::KMSKA>
+
+=item L<MSK|Datahub::Factory::Importer::MSK>
+
+=item L<VKC|Datahub::Factory::Importer::VKC>
+
+=item L<OAI|Datahub::Factory::Importer::OAI>
+
+=back
+
+Supported I<Exporter> plugins:
+
+=over
+
+=item L<Datahub|Datahub::Factory::Exporter::Datahub>
+
+=item L<LIDO|Datahub::Factory::Exporter::LIDO>
+
+=item L<YAML|Datahub::Factory::Exporter::YAML>
+
+=back
+
+=head3 Plugin configuration
+
+All plugins have their own configuration options in sections called
+C<[plugin_type_name]> where C<type> can be I<importer>, I<exporter>
+or I<fixer> and C<name> is the name of the plugin (see above).
+
+For a list of supported and required options, see the plugin documentation.
+
+The C<[plugin_fixer_Fix]> has two options: C<id_path> and C<file_name>.
+
+The C<id_path> option contains the path (in Fix syntax) of the identifier of
+each record in your data after the fix has been applied, but before it is
+submitted to the I<Exporter>. It is used for reporting and logging.
+
+C<file_name> points to the location of your Fix file.
+
+=head4 Example configuration file
+
+  [Importer]
+  plugin = Adlib
+
+  [Fixer]
+  plugin = Fix
+  id_path = 'administrativeMetadata.recordWrap.recordID.0._'
+
+  [Exporter]
+  plugin = Datahub
+
+  [plugin_importer_Adlib]
+  file_name = '/tmp/adlib.xml'
+  data_path = 'recordList.record.*'
+
+  [plugin_fixer_Fix]
+  file_name = '/tmp/msk.fix'
+  id_path = ''
+
+  [plugin_exporter_Datahub]
+  datahub_url = my.thedatahub.io
+  datahub_format = LIDO
+  oauth_client_id = datahub
+  oauth_client_secret = datahub
+  oauth_username = datahub
+  oauth_password = datahub
+
+=head1 AUTHORS
+
+=over
+
+=item Pieter De Praetere <pieter@packed.be>
+
+=item Matthias Vandermaesen <matthias.vandermaesen@vlaamsekunstcollectie.be>
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2016 - PACKED vzw, Vlaamse Kunstcollectie vzw
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GPLv3.
 
 =cut
-
