eb2ec99a2df0b1298595e6ab309ed52ad138e014
Pieter De Praetere <pieter@packed.be>
2017-05-11 13:44:25 +0200

    Updated Fixer::* documentation

--- lib/Datahub/Factory/Fixer/Fix.pm
+++ lib/Datahub/Factory/Fixer/Fix.pm
@@ -20,3 +20,53 @@ sub _build_fixer {
 }
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Datahub::Factory::Fixer::Fix - Execute fixes on a single record
+
+=head1 SYNOPSIS
+
+    use Datahub::Factory;
+
+    my $fix_options = {
+        file_name => '/tmp/my.fix'
+    }
+
+    my $fixer = Datahub::Factory->fixer('Fix')->new($fix_options);
+
+    $fixer->fixer->fix({'id' => 1});
+
+=head1 DESCRIPTION
+
+This module executes the fixes in C<file_name> for a single record.
+
+=head1 PARAMETERS
+
+=over
+
+=item C<file_name>
+
+Location of the fix file.
+
+=back
+
+=head1 AUTHORS
+
+Pieter De Praetere <pieter@packed.be>
+
+Matthias Vandermaesen <matthias.vandermaesen@vlaamsekunstcollectie.be>
+
+=head1 COPYRIGHT
+
+Copyright 2017 - PACKED vzw, Vlaamse Kunstcollectie vzw
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GPLv3.
+
+=cut
